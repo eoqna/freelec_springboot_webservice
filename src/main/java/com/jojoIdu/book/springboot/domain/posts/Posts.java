fe5f8d4bbe1d8846b4d1c40c8af07dd4e01d74aa
eoqna <eoqna@naver.com>
2023-01-04 22:25:48 +0900

    [eoqna] 23-01-04 Spring Boot와 Aws로 혼자 구현하는 웹서비스[145p]

--- src/main/java/com/jojoIdu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoIdu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoIdu.book.springboot.domain.posts;
 
+import com.jojoIdu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +29,11 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
 }
